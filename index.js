610a6b80a7aff48a5067eb56cd0ab017d9daf908
Daniel Cotic <d.cotic26@gmail.com>
2017-12-28 03:52:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = parseInt(Math.random() * 100)
+ var object = {[item]: price}
+ cart.push(object)
+ console.log(`${item} has been added to your cart.`);
+ return cart
 }
 
 function viewCart() {

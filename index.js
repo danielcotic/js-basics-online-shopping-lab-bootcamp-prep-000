f7dc2a91f79dc4908a85efa96be063d3962d539c
Daniel Cotic <d.cotic26@gmail.com>
2017-12-28 05:16:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,4 +54,5 @@ function placeOrder(cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.");
   }
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    cart.splice(0,cart.length)
 }

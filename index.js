854ccf00e12e806c2fe499b569f4ce0e5bf17406
Daniel Cotic <d.cotic26@gmail.com>
2017-12-28 05:02:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,9 +43,9 @@ function removeFromCart(item) {
   for(var i = 0; i < cart.length; i++) {
     if(Object.keys(cart[i]) == item) {
         cart.splice(i, 1);
+    } else {
+        return console.log("That item is not in your cart.");
     }
-    return console.log("That item is not in your cart.");
-
   }
 }
 

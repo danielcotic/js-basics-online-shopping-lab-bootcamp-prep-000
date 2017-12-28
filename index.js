852d9eeae9d1e8121a8bebdbf45cc972422fd732
Daniel Cotic <d.cotic26@gmail.com>
2017-12-28 04:49:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,10 +41,10 @@ function total() {
 
 function removeFromCart(item) {
   for(var i = 0; i < cart.length; i++) {
-    if (Object.keys(cart[i]) === item) {
+    if(Object.keys(cart[i]) === item) {
         cart.splice(i, 1);
+      }
     }
-  }
 }
 
 function placeOrder(cardNumber) {
